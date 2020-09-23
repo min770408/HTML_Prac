7a911cf6ed779133374fcafcc31fdd8a79138a40
min770408 <56524277+min770408@users.noreply.github.com>
2020-09-23 11:07:28 +0900

    Update scripts_first.js

--- scripts_first.js
+++ scripts_first.js
@@ -1,3 +1,9 @@
 function viewCurrDate() {
     document.getElementById('demo').innerHTML = Date();
+    
+    var name = prompt("이름을 입력하세요");
+    alert("입력한 이름은 " + name);
+    
+    var ret = confirm("다시 입력하시겠습니까?");
+    alert(ret);
 }
